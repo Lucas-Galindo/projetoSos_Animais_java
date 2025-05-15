1ff0d1ff9522edd9393528ce7660dbd206be35e6
Lucas Galindo <srlucasmtg@gmail.com>
2025-05-15 18:07:16 -0300

    mudança apos mudança d epasta

---
+++ app/src/main/java/sosanimais/com/example/app/model/PessoaInformacao.java
@@ -0,0 +1,61 @@
+package sosanimais.com.example.app.model;
+
+public class PessoaInformacao {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    //private Endereco adress;
+
+    public PessoaInformacao(String nome,String cpf,String telefone,String email){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;    
+        //this.adress = new Endereco(rua, numero, cep, complemento);
+    }
+
+    public PessoaInformacao(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//    public Endereco getAdress() {
+//        return adress;
+//    }
+
+//    public void setAdress(Endereco adress) {
+//        this.adress = adress;
+//    }
+}
